a10359c756e241cbd269b1ece44631151fb063d8
AriSyafri <diamondThink15@gmail.com>
2025-02-21 22:28:14 +0700

    menambah tampilan untuk blog dan home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -122,12 +122,14 @@
         <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home page</h1>
       </div>
     </header>
+
     <main>
       <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <!-- Your content -->
         <p>Welcome to my homepage</p>
       </div>
     </main>
+
   </div>
 
 
