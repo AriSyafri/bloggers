8ffade63579cf3950a841cdacb17f076a16ebda7
AriSyafri <diamondThink15@gmail.com>
2025-02-22 23:55:56 +0700

    membuat tampilan untuk template blog

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -2,5 +2,28 @@
 
     <x-slot:title>{{ $title }}</x-slot:title>
 
-    <h3 class="text-xl">Welcome to my blog</h3>
+    <article class="py-8 max-w-screen-md border-b border-gray-300">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Judul Artikel</h2>
+
+        <div class="text-base text-gray-500">
+            <a href="#">Ari Syafri</a> | 22 Februari 2025
+        </div>
+
+        <p class="my-4 font-light">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque eum ut, modi in reprehenderit voluptatem repellendus quam suscipit earum nobis aut unde provident quae, officia nam tempora dolor, non saepe rerum? Voluptas facere ab quaerat esse nulla, quo velit magnam quisquam aliquid, autem deleniti sed possimus? Inventore doloribus, autem exercitationem officiis ducimus debitis sapiente quia quod praesentium, itaque repellat non consectetur sit dolores sint eaque beatae suscipit, optio natus consequatur!</p>
+
+        <a href="#" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+    </article>
+
+
+    <article class="py-8 max-w-screen-md border-b border-gray-300">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Judul Artikel</h2>
+
+        <div class="text-base text-gray-500">
+            <a href="#">Ari Syafri</a> | 22 Februari 2025
+        </div>
+
+        <p class="my-4 font-light">Lorem epellendus quam suscipit earum nobis aut unde provident quae, officia nam tempora dolor, non saepe rerum? Voluptas facere ab quaerat esse nulla, quo velit magnam quisquam aliquid, autem deleniti sed possimus? Inventore doloribus, autem exercitationem officiis ducimus debitis sapiente quia quod praesentium, itaque repellat non consectetur sit doloresipsum dolor sit amet consectetur, adipisicing elit. Eaque eum ut, modi in reprehenderit voluptatem r sint eaque beatae suscipit, optio natus consequatur!</p>
+
+        <a href="#" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+    </article>
 </x-layout>
