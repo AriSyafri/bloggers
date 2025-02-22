9f08594ea5ffeb73d74ffadfe0512e10a1ebe9f4
AriSyafri <diamondThink15@gmail.com>
2025-02-22 22:59:07 +0700

    menambah title agar dinamis

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,7 +9,7 @@
     {{-- alphine js --}}
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
-    <title>Halaman home</title>
+    <title>{{ $title }}</title>
 
 </head>
 <body class="h-full">
