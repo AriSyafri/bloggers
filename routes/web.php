5e80ebc99d94ea7cf67d8f5369766c1a50fcb519
AriSyafri <diamondThink15@gmail.com>
2025-02-23 00:09:19 +0700

    mengganri blog menjadi posts

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,20 @@ Route::get('/about', function () {
     ]);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', [
+        'title' => 'Posts',
+        'posts' => [
+            'title' => 'Judul artikel 1',
+            'author' => 'Ari Syafri',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque eum ut, modi in reprehenderit voluptatem repellendus quam suscipit earum nobis aut unde provident quae, officia nam tempora dolor, non saepe rerum? Voluptas facere ab quaerat esse nulla, quo velit magnam quisquam aliquid, autem deleniti sed possimus? Inventore doloribus, autem exercitationem officiis ducimus debitis sapiente quia quod praesentium, itaque repellat non consectetur sit dolores sint eaque beatae suscipit, optio natus consequatur!'
+        ],
+        [
+            'title' => 'Judul artikel 2',
+            'author' => 'Ari Syafri',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque eum ut, modi in reprehenderit voluptatem repellendus quam suscipit earum nobis aut unde provident quae, officia nam tempora dolor, non saepe rerum? Voluptas facere ab quaerat esse nulla, quo velit magnam quisquam aliquid, autem deleniti sed possimus? '
+        ]
+    ]);
 });
 
 Route::get('/contact', function () {
