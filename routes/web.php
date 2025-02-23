522939b98f199417bc360b0a74c23d16e2354baf
AriSyafri <diamondThink15@gmail.com>
2025-02-23 09:37:34 +0700

    membuat template single post

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -35,6 +36,30 @@ Route::get('/posts', function () {
     ]);
 });
 
+
+Route::get('/posts/{id}', function($id) {
+    $posts = [
+        [
+            'id' => 1,
+            'title' => 'Judul artikel 1',
+            'author' => 'Ari Syafri',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque eum ut, modi in reprehenderit voluptatem repellendus quam suscipit earum nobis aut unde provident quae, officia nam tempora dolor, non saepe rerum? Voluptas facere ab quaerat esse nulla, quo velit magnam quisquam aliquid, autem deleniti sed possimus? Inventore doloribus, autem exercitationem officiis ducimus debitis sapiente quia quod praesentium, itaque repellat non consectetur sit dolores sint eaque beatae suscipit, optio natus consequatur!'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Judul artikel 2',
+            'author' => 'Ari',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. fafafafffaEaque eum ut, modi in reprehenderit voluptatem repellendus quam suscipit earum nobis aut unde provident quae, officia nam tempora dolor, non saepe rerum? Voluptas facere ab quaerat esse nulla, quo velit magnam quisquam aliquid, autem deleniti sed possimus? '
+        ]
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($id) {
+        return $post['id'] == $id;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
+
+});
 
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
