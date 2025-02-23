f93ef8b8a00add325abb21a75de2c536ebe04bcc
AriSyafri <diamondThink15@gmail.com>
2025-02-23 09:10:46 +0700

    membuat link untuk menuju single post

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,13 @@ Route::get('/posts', function () {
         'title' => 'Posts',
         'posts' => [
             [
+                'id' => 1,
                 'title' => 'Judul artikel 1',
                 'author' => 'Ari Syafri',
                 'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque eum ut, modi in reprehenderit voluptatem repellendus quam suscipit earum nobis aut unde provident quae, officia nam tempora dolor, non saepe rerum? Voluptas facere ab quaerat esse nulla, quo velit magnam quisquam aliquid, autem deleniti sed possimus? Inventore doloribus, autem exercitationem officiis ducimus debitis sapiente quia quod praesentium, itaque repellat non consectetur sit dolores sint eaque beatae suscipit, optio natus consequatur!'
             ],
             [
+                'id' => 2,
                 'title' => 'Judul artikel 2',
                 'author' => 'Ari',
                 'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. fafafafffaEaque eum ut, modi in reprehenderit voluptatem repellendus quam suscipit earum nobis aut unde provident quae, officia nam tempora dolor, non saepe rerum? Voluptas facere ab quaerat esse nulla, quo velit magnam quisquam aliquid, autem deleniti sed possimus? '
