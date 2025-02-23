823bfe9a8381beb1e9d6a69b38e299113a75ac63
AriSyafri <diamondThink15@gmail.com>
2025-02-23 09:41:48 +0700

    mengganti id dengan slug

--- routes/web.php
+++ routes/web.php
@@ -21,12 +21,14 @@ Route::get('/posts', function () {
         'posts' => [
             [
                 'id' => 1,
+                'slug' => 'judul-artikel-1',
                 'title' => 'Judul artikel 1',
                 'author' => 'Ari Syafri',
                 'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque eum ut, modi in reprehenderit voluptatem repellendus quam suscipit earum nobis aut unde provident quae, officia nam tempora dolor, non saepe rerum? Voluptas facere ab quaerat esse nulla, quo velit magnam quisquam aliquid, autem deleniti sed possimus? Inventore doloribus, autem exercitationem officiis ducimus debitis sapiente quia quod praesentium, itaque repellat non consectetur sit dolores sint eaque beatae suscipit, optio natus consequatur!'
             ],
             [
                 'id' => 2,
+                'slug' => 'judul-artikel-2',
                 'title' => 'Judul artikel 2',
                 'author' => 'Ari',
                 'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. fafafafffaEaque eum ut, modi in reprehenderit voluptatem repellendus quam suscipit earum nobis aut unde provident quae, officia nam tempora dolor, non saepe rerum? Voluptas facere ab quaerat esse nulla, quo velit magnam quisquam aliquid, autem deleniti sed possimus? '
@@ -37,24 +39,26 @@ Route::get('/posts', function () {
 });
 
 
-Route::get('/posts/{id}', function($id) {
+Route::get('/posts/{slug}', function($slug) {
     $posts = [
         [
             'id' => 1,
+            'slug' => 'judul-artikel-1',
             'title' => 'Judul artikel 1',
             'author' => 'Ari Syafri',
             'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque eum ut, modi in reprehenderit voluptatem repellendus quam suscipit earum nobis aut unde provident quae, officia nam tempora dolor, non saepe rerum? Voluptas facere ab quaerat esse nulla, quo velit magnam quisquam aliquid, autem deleniti sed possimus? Inventore doloribus, autem exercitationem officiis ducimus debitis sapiente quia quod praesentium, itaque repellat non consectetur sit dolores sint eaque beatae suscipit, optio natus consequatur!'
         ],
         [
             'id' => 2,
+            'slug' => 'judul-artikel-2',
             'title' => 'Judul artikel 2',
             'author' => 'Ari',
             'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. fafafafffaEaque eum ut, modi in reprehenderit voluptatem repellendus quam suscipit earum nobis aut unde provident quae, officia nam tempora dolor, non saepe rerum? Voluptas facere ab quaerat esse nulla, quo velit magnam quisquam aliquid, autem deleniti sed possimus? '
         ]
     ];
 
-    $post = Arr::first($posts, function ($post) use ($id) {
-        return $post['id'] == $id;
+    $post = Arr::first($posts, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
     });
 
     return view('post', ['title' => 'Single Post', 'post' => $post]);
