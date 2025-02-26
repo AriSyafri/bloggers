39e9eac03c50664f9c67885bb55eac9832ac613e
AriSyafri <diamondThink15@gmail.com>
2025-02-26 15:42:16 +0700

    mengganti fitur menampilkan waktu menggunakan diffForHumans()

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -9,7 +9,7 @@
             </a>
 
             <div class="text-base text-gray-500">
-                <a href="#">{{ $post['author'] }}</a> | {{ $post->created_at->format('j F Y') }}
+                <a href="#">{{ $post['author'] }}</a> | {{ $post->created_at->diffForHumans() }}
             </div>
 
             <p class="my-4 font-light">{{ Str::limit($post['body'], 150) }}</p>
