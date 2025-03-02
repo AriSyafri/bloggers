be8855c686d41e628f86f7f7e8100778396a8574
AriSyafri <diamondThink15@gmail.com>
2025-03-02 13:00:39 +0700

    menghapus bagian admin

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -47,11 +47,4 @@ class UserFactory extends Factory
             'email_verified_at' => null,
         ]);
     }
-
-    public function admin(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'is_admin' => true,
-        ]);
-    }
 }
