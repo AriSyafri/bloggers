f73cd4e0112d7959c94e8ce219e8a97350fec3e4
AriSyafri <diamondThink15@gmail.com>
2025-03-04 13:33:43 +0700

    membuat tabel catergory, model, dan factory

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,11 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
     /** @use HasFactory<\Database\Factories\CategoryFactory> */
     use HasFactory;
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class, 'author_id');
+    }
 }
--- database/migrations/2025_02_26_094417_create_posts_table.php
+++ database/migrations/2025_02_26_094417_create_posts_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->foreignId('author_id')->constrained(
                 table: 'users', indexName: 'post_author_id'
             );
+            $table->foreignId('category_id')->constrained(
+                table: 'categories', indexName: 'post_category_id'
+            );
             $table->string('slug')->unique();
             $table->text('body');
             $table->timestamps();
