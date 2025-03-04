6b50c9c3b605763fe5837458f32dc6afd8e2965d
AriSyafri <diamondThink15@gmail.com>
2025-03-04 14:26:26 +0700

    menambah route untuk menampilkan categori

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
 use App\Models\Post;
+use App\Models\Category;
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
@@ -30,7 +31,13 @@ Route::get('/posts/{post:slug}', function(Post $post) {
 });
 
 Route::get('/authors/{user:username}', function(User $user) {
-    return view('posts', ['title' => count($user->posts) . ' Article by ' . $user->name, 'posts' => $user->posts]);
+    return view('posts', ['title' => count($user->posts) . ' Article by ' . $user->name,
+                            'posts' => $user->posts]);
+});
+
+Route::get('/categories/{category:slug}', function(Category $category) {
+    return view('posts', ['title' => 'Articles in : ' . $category->name,
+                            'posts' => $category->posts]);
 });
 
 Route::get('/contact', function () {
