5a3ebc7e03de92fb20a1df2247c45f76d8fc254c
AriSyafri <diamondThink15@gmail.com>
2025-03-05 11:42:55 +0700

    memiindahkan eager loading menjadi default di model

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,11 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    $post = Post::with(['author', 'category'])->latest()->get();
+    // $post = Post::with(['author', 'category'])->latest()->get();
+    $posts = Post::latest()->get();
     return view('posts', [
         'title' => 'Posts',
-        'posts' => $post
+        'posts' => $posts
     ]);
 });
 
@@ -33,19 +34,15 @@ Route::get('/posts/{post:slug}', function(Post $post) {
 });
 
 Route::get('/authors/{user:username}', function(User $user) {
-
-    $posts = $user->posts->load('category', 'author');
-
-    return view('posts', ['title' => count($posts) . ' Article by ' . $user->name,
-                            'posts' => $posts]);
+    // $posts = $user->posts->load('category', 'author');
+    return view('posts', ['title' => count($user->posts) . ' Article by ' . $user->name,
+                            'posts' => $user->posts]);
 });
 
 Route::get('/categories/{category:slug}', function(Category $category) {
-
-    $posts = $category->posts->load('category', 'author');
-
+    // $posts = $category->posts->load('category', 'author');
     return view('posts', ['title' => 'Articles in : ' . $category->name,
-                            'posts' => $posts]);
+                            'posts' => $category->posts]);
 });
 
 Route::get('/contact', function () {
