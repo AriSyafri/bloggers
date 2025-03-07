23c3a61d08c045a1daa8c123e5545c1996f79e6f
AriSyafri <diamondThink15@gmail.com>
2025-03-08 00:31:02 +0700

    menambah fitur pagination

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -32,8 +32,9 @@
         </div>
     </div>
 
+    {{ $posts->links() }}
 
-        <div class="py-4 px-4 mx-auto max-w-screen-xl lg:py-4 lg:px-0">
+        <div class="my-4 py-4 px-4 mx-auto max-w-screen-xl lg:py-4 lg:px-0">
             <div class="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
     @forelse ($posts as $post)
 
@@ -77,6 +78,8 @@
             @endforelse
             </div>
         </div>
+
+        {{ $posts->links() }}
 
 
 </x-layout>
