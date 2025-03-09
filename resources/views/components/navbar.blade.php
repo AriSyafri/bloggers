dea10c3e989a08cb588084f5f945a9d6a69d6fdd
AriSyafri <diamondThink15@gmail.com>
2025-03-09 15:31:12 +0700

    menambah fitur my dashboard, logout, dan login di navbar desktop

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -20,7 +20,35 @@
           <div class="ml-4 flex items-center md:ml-6">
 
             <!-- Profile dropdown -->
-            <x-nav-link href="/login">Login</x-nav-link>
+            <div class="relative ml-3">
+              <div>
+                <button type="button" @click="isOpen = !isOpen" class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                  <span class="absolute -inset-1.5"></span>
+                  <span class="sr-only">Open user menu</span>
+                  <img class="size-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                </button>
+              </div>
+
+
+              <div x-show="isOpen"
+              x-transition:enter="transition ease-out duration-100 transform"
+              x-transition:enter-start="opacity-0 scale-95"
+              x-transition:enter-end="opacity-100 scale-100"
+              x-transition:leave="transition ease-in duration-75 transform"
+              x-transition:leave-start="opacity-100 scale-100"
+              x-transition:leave-end="opacity-0 scale-95"
+              class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 ring-1 shadow-lg ring-black/5 focus:outline-hidden" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+                <!-- Active: "bg-gray-100 outline-hidden", Not Active: "" -->
+                @auth
+                    <p class="block px-4 py-2 text-sm text-gray-700">Welcome Back, {{ auth()->user()->name }} </p>
+                    <a href="/dashboard" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">My Dashboard</a>
+                    <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Logout</a>
+
+                @else
+                    <a href="/login" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Login</a>
+                @endauth
+              </div>
+            </div>
           </div>
         </div>
         <div class="-mr-2 flex md:hidden">
@@ -55,6 +83,10 @@
         <div class="flex items-center px-5">
           <div class="shrink-0">
             <img class="size-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+          </div>
+          <div class="ml-3">
+            <div class="text-base/5 font-medium text-white">Tom Cook</div>
+            <div class="text-sm font-medium text-gray-400">tom@example.com</div>
           </div>
         </div>
         <div class="mt-3 space-y-1 px-2">
