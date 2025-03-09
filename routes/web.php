e0c16f593f5b1d1b44055ab577106f63d9953ddc
AriSyafri <diamondThink15@gmail.com>
2025-03-09 15:15:59 +0700

    menambah middle ware untuk login

--- routes/web.php
+++ routes/web.php
@@ -50,7 +50,7 @@ Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
 });
 
-Route::get('/login', [LoginController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index'])->middleware('guest');
 Route::post('/login', [LoginController::class, 'authenticate']);
 
 Route::get('/register', [RegisterController::class, 'index']);
