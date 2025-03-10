00926280a3c1b6efe60e7c44eaa0e2280370cb26
AriSyafri <diamondThink15@gmail.com>
2025-03-10 23:31:30 +0700

    menambah icon di logout dan sidebar dashboard

--- resources/views/dashboard/components/navbar.blade.php
+++ resources/views/dashboard/components/navbar.blade.php
@@ -350,12 +350,18 @@
             class="py-1 text-gray-700 dark:text-gray-300"
             aria-labelledby="dropdown"
           >
-            <li>
-              <form action="/logout" method="post">
-                  @csrf
-                  <button type="submit" class="block px-4 py-2 text-sm text-gray-700">Logout</button>
-              </form>
-            </li>
+          <li>
+            <form action="/logout" method="post">
+              @csrf
+              <button type="submit" class="flex items-center px-4 py-2 text-sm text-gray-700">
+                <svg class="w-5 h-5 text-gray-400 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 12H8m12 0-4 4m4-4-4-4M9 4H7a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h2"/>
+                </svg>
+                <span class="ml-2">Logout</span>
+              </button>
+            </form>
+          </li>
+
           </ul>
         </div>
       </div>
--- resources/views/dashboard/components/sidebar.blade.php
+++ resources/views/dashboard/components/sidebar.blade.php
@@ -39,20 +39,10 @@
                   href="/dashboard"
                   class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group"
                 >
-                  <svg
-                    aria-hidden="true"
-                    class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path>
-                    <path
-                      fill-rule="evenodd"
-                      d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z"
-                      clip-rule="evenodd"
-                    ></path>
+                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                    <path fill-rule="evenodd" d="M11.293 3.293a1 1 0 0 1 1.414 0l6 6 2 2a1 1 0 0 1-1.414 1.414L19 12.414V19a2 2 0 0 1-2 2h-3a1 1 0 0 1-1-1v-3h-2v3a1 1 0 0 1-1 1H7a2 2 0 0 1-2-2v-6.586l-.293.293a1 1 0 0 1-1.414-1.414l2-2 6-6Z" clip-rule="evenodd"/>
                   </svg>
+
                   <span class="ml-3">Dashboard</span>
                 </a>
               </li>
