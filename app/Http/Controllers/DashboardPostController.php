2302235f04516b03092442b4c62f7a98cd50d361
AriSyafri <diamondThink15@gmail.com>
2025-03-15 10:37:12 +0700

    menambah combo box untuk category di post

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use \Cviebrock\EloquentSluggable\Services\SlugService;
@@ -33,6 +34,7 @@ class DashboardPostController extends Controller
     {
         return view('dashboard.posts.create', [
             'title' => 'Create Post',
+            'categories' => Category::all(),
         ]);
     }
 
