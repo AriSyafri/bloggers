77b9ca0c558c84e954b46670f0de1b9a109190bb
AriSyafri <diamondThink15@gmail.com>
2025-03-21 01:25:08 +0700

    melakukan penyimpanan data yang di update

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -22,7 +22,7 @@
                     </div>
                 </address>
                 <a href="/dashboard/posts" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">&laquo; back to all posts</a>
-                <a href="#" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800">Edit</a>
+                <a href="/dashboard/posts/{{ $post->slug }}/edit" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800">Edit</a>
 
                 <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="inline">
                     @method('delete')
