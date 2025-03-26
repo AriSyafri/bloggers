19b57937625cde4e81c5b87201f7e05cf503fdb7
AriSyafri <diamondThink15@gmail.com>
2025-03-26 10:42:28 +0700

    menambah fitur agar otorisasi pada side bar agar tidak muncul selain admin dan mengembalikan middleware route category

--- app/Http/Controllers/AdminCategoryController.php
+++ app/Http/Controllers/AdminCategoryController.php
@@ -19,7 +19,7 @@ class AdminCategoryController extends Controller
 
     public function index()
     {
-        $this->authorize('admin');
+
         return view('dashboard.categories.index', [
             'title' => 'Category',
             'categories' => Category::all()
--- routes/web.php
+++ routes/web.php
@@ -74,4 +74,4 @@ Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'check
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
 
 // Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')->middleware('admin');
-Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show');
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')->middleware('admin');
