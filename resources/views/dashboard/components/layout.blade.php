cdb58dd60385ab985f0ddc913ec24510698af39b
AriSyafri <diamondThink15@gmail.com>
2025-03-31 02:59:42 +0700

    menambah icon title

--- resources/views/dashboard/components/layout.blade.php
+++ resources/views/dashboard/components/layout.blade.php
@@ -9,6 +9,8 @@
     {{-- alphine js --}}
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
+    <link rel="icon" type="image/png" href="{{ asset('img/logo-transparan.png') }}">
+
     {{-- trix code editor --}}
     <link rel="stylesheet" type="text/css" href="https://unpkg.com/trix@2.0.8/dist/trix.css">
     <script type="text/javascript" src="https://unpkg.com/trix@2.0.8/dist/trix.umd.min.js"></script>
@@ -19,6 +21,7 @@
             display:none;
         }
     </style>
+
 
     <title>{{ $title }}</title>
 
