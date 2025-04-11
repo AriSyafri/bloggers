da0324d435a93790366909c3846677b083908150
AriSyafri <diamondThink15@gmail.com>
2025-04-11 23:47:26 +0700

    mengganti icon default

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -46,7 +46,7 @@
 
                         <div class="flex items-center space-x-6">
                             <div class="shrink-0">
-                                <img class="h-16 w-16 object-cover rounded-full" src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1361&q=80" alt="Current profile photo" />
+                                <img class="h-16 w-16 object-cover rounded-full" src="/img/icon-user.jpg" alt="Current profile photo" />
                             </div>
                             <label class="block">
                                 <span>Choose profile photo</span>
