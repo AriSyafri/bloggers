f2e89a9431e1cb9d1f54df88aa67b233be52f010
AriSyafri <diamondThink15@gmail.com>
2025-04-12 00:35:52 +0700

    memperbaiki button register

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -65,7 +65,7 @@
                         </div>
 
 
-                        <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Login</button>
+                        <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Register</button>
                     </form>
                     <p class="text-sm font-light text-gray-500 dark:text-gray-400">
                         have an account yet? <a href="/login" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login</a>
