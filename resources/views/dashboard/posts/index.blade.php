313d0e6bd4dff48da7a2dadf3168d78855ad1244
AriSyafri <diamondThink15@gmail.com>
2025-04-18 13:31:24 +0700

    memperbaiki label posts

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -32,7 +32,7 @@
                 <form action="/dashboard/posts" method="GET">
                     <div class="items-center mx-auto mb-2 space-y-4 max-w-screen-sm sm:flex sm:space-y-0">
                         <div class="relative w-full">
-                            <label for="email" class="hidden mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Search</label>
+                            <label for="search" class="hidden mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Search</label>
                             <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                                 <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/>
