86522ce77484940664ea73bc9e2e80fae008f46c
AriSyafri <diamondThink15@gmail.com>
2025-04-19 09:29:41 +0700

    mengganti tampilan untuk user

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,20 +2,21 @@
 
     <x-slot:title>{{ $title }}</x-slot:title>
 
-    <div class="items-center py-4 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
+    <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+            <img src="{{ auth()->user()->image ? asset('storage/' . auth()->user()->image) : '/img/icon-user.jpg' }}" alt="Image Profile">
+        </div>
 
-        <img class="w-full dark:hidden"
-            src="{{ auth()->user()->image ? asset('storage/' . auth()->user()->image) : '/img/icon-user.jpg' }}"
-            alt="user img" />
-        <div class="mt-4 md:mt-0">
-            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Name : {{ auth()->user()->name }}</h2>
-            <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">Username : {{ auth()->user()->username }}</p>
-            <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">Email : {{ auth()->user()->email }}</p>
-            <a href="/dashboard/users/{{ auth()->user()->slug }}/edit" class="inline-flex items-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900">
-                Change user
-                <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+        <div class="mr-auto px-10 place-self-center lg:col-span-7">
+            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">{{ auth()->user()->name }}</h1>
+            <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Username : {{ auth()->user()->username }}</p>
+            <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Name : {{ auth()->user()->email }}</p>
+            <a href="/dashboard/users/{{ auth()->user()->slug }}/edit" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                Change User
+                <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </a>
         </div>
+
     </div>
 
 
