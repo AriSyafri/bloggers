244f451ef50757d2d7518afbe7532b6c41fbdabb
AriSyafri <diamondThink15@gmail.com>
2025-05-13 17:32:13 +0700

    menambah angka 1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
 
-    <x-slot:title>Selamat Datang kucrut</x-slot:title>
+    <x-slot:title>Selamat Datang kucrut 1</x-slot:title>
 
     @auth
         <h3 class="text-xl">Jumpa Lagi {{ auth()->user()->name }}</h3>
